a265079929ad2f1ee1fba3f2dc80e218ed6109f4
alexandre.matias <axdmatias310@gmail.com>
2021-11-26 08:28:03 -0300

    data

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ if (idade >= 18){
 }else{
     console.log(`Não possui idade, ${idade} anos`)
 }
+let ano = 2021
+let mes = 11
+let dia = 28
 
+let data = new Date()
+let dataEvento = new Date(2021, 11, 27)
+let testeData = Date.UTC()
+
+console.log(data)
+console.log(dataEvento)
+
+if (data > dataEvento){
+    console.log("Compra invalida, expirada a data de compra")
+}else{
+    console.log("Compra feita")
+}
+
+console.log(testeData)

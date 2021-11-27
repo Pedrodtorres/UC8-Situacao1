dfe90f5120ab2258a16bc7a01fecf2ee06290d23
alexandre.matias <axdmatias310@gmail.com>
2021-11-27 05:43:50 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
-var idade = 32
-
-if (idade >= 18){
-    console.log(`Idade permitida, ${idade} anos`)
-}else{
-    console.log(`Não possui idade, ${idade} anos`)
-}
-let anoEvento=2021
-let mesEvento=10
+let idade = 1
+let anoEvento=2022
+let mesEvento=12
 let diaEvento=28
-
-let data = new Date()
+let nomeCadastrados = ['Igor','Carlos', 'Ricardo', 'Caio'] 
+let nomePalestrantes = ['Ana Paula', 'Roberto']
+let qtdParticipantes = nomeCadastrados.length;
+let limitePart = 100
+let dataAtual = new Date()
 let dataEvento = new Date(anoEvento, mesEvento-1, diaEvento) //Valor dos mês -1 pois os meses estão em vetores de 0 a 11
 
-console.log(data)
-console.log(dataEvento)
+if (dataAtual < dataEvento && qtdParticipantes < limitePart &&  idade > 18) {
+    console.log(`Compra feita, a data do evento será: ${diaEvento}/${mesEvento}/${anoEvento}`)  
+}else{      
+        if (dataAtual > dataEvento){
+            console.log(`Compra invalida, data do evento já ocorreu:${diaEvento}/${mesEvento}/${anoEvento} `)
+        }
+            if (qtdParticipantes > limitePart){
+                console.log('O evento já está com limite máximo de pessoas.')
+            }
+                if(idade < 18){
+                    console.log('Menores de idade não são permitidos no evento.')
+                }
+    }
 
-if (data > dataEvento){
-    console.log(`Data de compras se encerraram na data de: ${diaEvento}/${mesEvento}/${anoEvento}`)
-}else{
-    console.log("Compra feita")
-}
+
+
 

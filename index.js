df43eec0ce1a12215633f857fe027b84e198675b
alexandre.matias <axdmatias310@gmail.com>
2021-11-28 03:12:23 -0300

    att listas

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let idade = 1
+let idade = 19
 let anoEvento=2022
 let mesEvento=12
 let diaEvento=28
 let nomeCadastrados = ['Igor','Carlos', 'Ricardo', 'Caio'] 
 let nomePalestrantes = ['Ana Paula', 'Roberto']
 let qtdParticipantes = nomeCadastrados.length;
+let qtdPalestrantes = nomePalestrantes.length;
 let limitePart = 100
 let dataAtual = new Date()
 let dataEvento = new Date(anoEvento, mesEvento-1, diaEvento) //Valor dos mês -1 pois os meses estão em vetores de 0 a 11
@@ -22,7 +23,16 @@ if (dataAtual < dataEvento && qtdParticipantes < limitePart &&  idade > 18) {
                     console.log('Menores de idade não são permitidos no evento.')
                 }
     }
-
+    console.log("Lista de cadastrados")
+for (let indice = 0; indice < qtdParticipantes; indice++){
+    const listaParticipantes = nomeCadastrados[indice];
+    console.log(listaParticipantes)
+}
+    console.log("Lista de Palestrantes")
+for (let indice = 0; indice < qtdPalestrantes; indice++){
+    const listaPalestrantes = nomePalestrantes[indice];
+    console.log(listaPalestrantes);
+}
 
 
 
